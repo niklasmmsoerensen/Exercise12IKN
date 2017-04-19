aaf867858b9c0521ce7d95227ace7f01b390a29a
Niklas <niklasmms7@hotmail.com>
2017-04-19 10:57:31 +0200

    Implementering af Link send function.

--- Link/Link.cs
+++ Link/Link.cs
@@ -65,8 +65,47 @@ namespace Linklaget
 		/// </param>
 		public void send (byte[] buf, int size)
 		{
-	    	// TO DO Your own code
-		}
+            // TO DO Your own code
+            int i = 1;
+            char c;
+            byte[] sendBuf = new byte[size * 2 + 2];
+
+            sendBuf[0] = DELIMITER;
+
+            foreach (var bufferByte in buf)
+            {
+                if (bufferByte == (byte)'A')
+                {
+                    sendBuf[i] = (byte)'B';
+                    i++;
+                    sendBuf[i] = (byte)'C';
+                }
+                else if (bufferByte == (byte)'B')
+                {
+                    sendBuf[i] = (byte)'B';
+                    i++;
+                    sendBuf[i] = (byte)'D';
+                }
+                else
+                {
+                    sendBuf[i] = bufferByte;
+                }
+
+                i++;
+            }
+            sendBuf[i] = DELIMITER;
+
+            foreach (var x in buf)
+            {
+                Console.Write(Convert.ToChar(x));
+            }
+            Console.WriteLine();
+            foreach (var x in sendBuf)
+            {
+                Console.Write(Convert.ToChar(x));
+            }
+            Console.WriteLine();
+        }
 
 		/// <summary>
 		/// Receive the specified buf and size.
